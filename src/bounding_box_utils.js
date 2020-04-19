61211e4e66af1193e72febc59d2142cbe16eec7f
Pablo Bianciotto <bianciottopablo@gmail.com>
2020-04-19 13:39:12 -0300

    Fix bounding box bug

--- src/bounding_box_utils.js
+++ src/bounding_box_utils.js
@@ -2,10 +2,10 @@ const boundingBoxToStyle = (container, element, minX, minY, maxX, maxY) => {
   const containerWidth = container.clientWidth,
         containerHeight = container.clientHeight
   
-  element.style.left = `${ 100 * minX / containerWidth }%`
-  element.style.right = `${ 100 * (1 - maxX / containerWidth) }%`
-  element.style.top = `${ 100 * minY / containerHeight }%`
-  element.style.bottom = `${ 100 * (1 - maxY / containerHeight) }%`
+  element.style.left = `${ Math.min(95, Math.max(0, 100 * minX / containerWidth)) }%`
+  element.style.right = `${ Math.min(95, Math.max(0, 100 * (1 - maxX / containerWidth))) }%`
+  element.style.top = `${ Math.min(95, Math.max(0, 100 * minY / containerHeight)) }%`
+  element.style.bottom = `${ Math.min(95, Math.max(0, 100 * (1 - maxY / containerHeight))) }%`
 }
 
 module.exports = {
