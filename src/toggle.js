811ba4c6b2266871f40292218bd4606fb0b25ac9
Pablo Bianciotto <bianciottopablo@gmail.com>
2022-11-19 13:38:20 -0300

    Fix toggle tooltip

--- src/toggle.js
+++ src/toggle.js
@@ -4,25 +4,30 @@ module.exports = _ => {
         DISABLED_ICON_PATH = 'M 3 5 L 3 23 L 8 23 L 8 28.078125 L 14.351563 23 L 29 23 L 29 5 Z M 5 7 L 27 7 L 27 21 L 13.648438 21 L 10 23.917969 L 10 21 L 5 21 Z',
         toggle = document.createElement('div')
   toggle.id = "anu-chat-overlay-toggle"
-  toggle.className = 'Layout-sc-nxg1ff-0 ScAttachedTooltipWrapper-sc-v8mg6d-0 hOVSHb'
+  toggle.className = 'Layout-sc-1xcs6mc-0'
   toggle.setAttribute('aria-describedby', 'anu-chat-overlay-toggle-tooltip')
   toggle.innerHTML = `
-    <button class="ScCoreButton-sc-1qn4ixc-0 cgCHoV ScButtonIcon-sc-o7ndmn-0 kwoFXD" aria-label="Anu Chat Overlay">
-      <div class="ButtonIconFigure-sc-1ttmz5m-0 fbCCvx">
-        <div class="ScIconLayout-sc-1bgeryd-0 dPgmNu">
-          <div class="ScAspectRatio-sc-1sw3lwy-1 dJaMsL tw-aspect">
-            <div class="ScAspectSpacer-sc-1sw3lwy-0 giUbvo"></div>
-            <svg width="100%" height="100%" version="1.1" viewBox="0 0 32 32" x="0px" y="0px" class="ScIconSVG-sc-1bgeryd-1 hcjcOH">
-              <g>
-                <path class="active" fill-rule="evenodd" d="${ ENABLED_ICON_PATH }" clip-rule="evenodd"></path>
-                <path class="inactive" fill-rule="evenodd" d="${ DISABLED_ICON_PATH }" clip-rule="evenodd"></path>
-              </g>
-            </svg>
+    <div class="Layout-sc-1xcs6mc-0 ScLayoutCssVars-sc-10awzi2-1 gdUPAO"></div>
+    <div class="Layout-sc-1xcs6mc-0">
+      <div aria-describedby="anu-chat-overlay-toggle-tooltip" class="Layout-sc-1xcs6mc-0 ScAttachedTooltipWrapper-sc-1ems1ts-0 deuUPa">
+        <button class="ScCoreButton-sc-ocjdkq-0 dNGoHt ScButtonIcon-sc-9yap0r-0 eekshR" aria-label="Anu Chat Overlay" data-a-target="anu-chat-overlay-toggle-button">
+          <div class="ButtonIconFigure-sc-1emm8lf-0 bxQRSJ">
+            <div class="ScIconLayout-sc-1q25cff-0 cMWGQu">
+              <div class="ScAspectRatio-sc-18km980-1 hTTohL tw-aspect">
+                <div class="ScAspectSpacer-sc-18km980-0 kiiGFY"></div>
+                <svg width="100%" height="100%" version="1.1" viewBox="0 0 32 32" x="0px" y="0px" class="ScIconSVG-sc-1q25cff-1 dSicFr">
+                  <g>
+                    <path class="active" fill-rule="evenodd" d="${ ENABLED_ICON_PATH }" clip-rule="evenodd"></path>
+                    <path class="inactive" fill-rule="evenodd" d="${ DISABLED_ICON_PATH }" clip-rule="evenodd"></path>
+                  </g>
+                </svg>
+              </div>
+            </div>
           </div>
-        </div>
+        </button>
+        <div class="ScAttachedTooltip-sc-1ems1ts-1 dkeAyr tw-tooltip" data-a-target="tw-tooltip-label" role="tooltip" id="anu-chat-overlay-toggle-tooltip" direction="top">Anu Chat Overlay</div>
       </div>
-    </button>
-    <div class="ScAttachedTooltip-sc-v8mg6d-1 kqEiJv tw-tooltip" data-a-target="tw-tooltip-label" role="tooltip" id="anu-chat-overlay-toggle-tooltip" direction="top">Anu Chat Overlay</div>
+    </div>
   `
   return toggle
 }
