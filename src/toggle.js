b8b0d56636565dda094faa9d9fb7811ecca185fb
Pablo Bianciotto <bianciottopablo@gmail.com>
2023-02-05 13:32:13 -0300

    Fix tooltip and extension being injected in dashboard pages

--- src/toggle.js
+++ src/toggle.js
@@ -4,29 +4,26 @@ module.exports = _ => {
         DISABLED_ICON_PATH = 'M 3 5 L 3 23 L 8 23 L 8 28.078125 L 14.351563 23 L 29 23 L 29 5 Z M 5 7 L 27 7 L 27 21 L 13.648438 21 L 10 23.917969 L 10 21 L 5 21 Z',
         toggle = document.createElement('div')
   toggle.id = "anu-chat-overlay-toggle"
-  toggle.className = 'Layout-sc-1xcs6mc-0'
+  toggle.className = 'Layout-sc-1xcs6mc-0 ScAttachedTooltipWrapper-sc-1ems1ts-0 deuUPa'
   toggle.setAttribute('aria-describedby', 'anu-chat-overlay-toggle-tooltip')
   toggle.innerHTML = `
-    <div class="Layout-sc-1xcs6mc-0 ScLayoutCssVars-sc-10awzi2-1 gdUPAO"></div>
-    <div class="Layout-sc-1xcs6mc-0">
-      <div aria-describedby="anu-chat-overlay-toggle-tooltip" class="Layout-sc-1xcs6mc-0 ScAttachedTooltipWrapper-sc-1ems1ts-0 deuUPa">
-        <button class="ScCoreButton-sc-ocjdkq-0 dNGoHt ScButtonIcon-sc-9yap0r-0 eekshR" aria-label="Anu Chat Overlay" data-a-target="anu-chat-overlay-toggle-button">
-          <div class="ButtonIconFigure-sc-1emm8lf-0 bxQRSJ">
-            <div class="ScIconLayout-sc-1q25cff-0 cMWGQu">
-              <div class="ScAspectRatio-sc-18km980-1 hTTohL tw-aspect">
-                <div class="ScAspectSpacer-sc-18km980-0 kiiGFY"></div>
-                <svg width="100%" height="100%" version="1.1" viewBox="0 0 32 32" x="0px" y="0px" class="ScIconSVG-sc-1q25cff-1 dSicFr">
-                  <g>
-                    <path class="active" fill-rule="evenodd" d="${ ENABLED_ICON_PATH }" clip-rule="evenodd"></path>
-                    <path class="inactive" fill-rule="evenodd" d="${ DISABLED_ICON_PATH }" clip-rule="evenodd"></path>
-                  </g>
-                </svg>
-              </div>
-            </div>
+    <button class="ScCoreButton-sc-ocjdkq-0 dNGoHt ScButtonIcon-sc-9yap0r-0 eekshR" aria-label="Anu Chat Overlay">
+      <div class="ButtonIconFigure-sc-1emm8lf-0 bxQRSJ">
+        <div class="ScIconLayout-sc-1q25cff-0 cMWGQu">
+          <div class="ScAspectRatio-sc-18km980-1 hTTohL tw-aspect">
+            <div class="ScAspectSpacer-sc-18km980-0 kiiGFY"></div>
+            <svg width="100%" height="100%" version="1.1" viewBox="0 0 32 32" x="0px" y="0px" class="ScIconSVG-sc-1q25cff-1 dSicFr">
+              <g>
+                <path class="active" fill-rule="evenodd" d="${ ENABLED_ICON_PATH }" clip-rule="evenodd"></path>
+                <path class="inactive" fill-rule="evenodd" d="${ DISABLED_ICON_PATH }" clip-rule="evenodd"></path>
+              </g>
+            </svg>
           </div>
-        </button>
-        <div class="ScAttachedTooltip-sc-1ems1ts-1 dkeAyr tw-tooltip" data-a-target="tw-tooltip-label" role="tooltip" id="anu-chat-overlay-toggle-tooltip" direction="top">Anu Chat Overlay</div>
+        </div>
       </div>
+    </button>
+    <div class="ScAttachedTooltip-sc-1ems1ts-1 doBxUm tw-tooltip" data-a-target="anu-chat-overlay-toggle-button" role="tooltip" direction="top">
+      Anu Chat Overlay
     </div>
   `
   return toggle
